79e1aeada357ca1b44da16a3860d012dcf989142
SAIRAM BOTLA <sairambotla0416@gmail.com>
2023-11-22 16:47:53 +0530

    first commit

---
+++ portfolio script.js
@@ -0,0 +1,10 @@
+// the below cdn links is for text to auto-type
+/* <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script> */
+/* <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script> */
+
+var type = new Typed(".auto-type",{
+    strings: ["HTML (HyperText Markup Language)", "CSS (Cascading Style Sheets)","Bootstrap","JavaScript"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true 
+})
